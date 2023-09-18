15b6f9cb71d4953c9038b0820855bced25df0e75
TI <pcp@cemag.com.br>
2023-09-18 15:03:34 -0300

    cadastro e equip envio backend

--- app.py
+++ app.py
@@ -1,28 +1,101 @@
 from flask import Flask,render_template, redirect, url_for, request, session, flash, make_response, Response
+import psycopg2  # pip install psycopg2
+import psycopg2.extras
+from functools import wraps
+import requests
 
 app = Flask(__name__)
 app.secret_key = "calibracao"
 
+DB_HOST = "database-1.cdcogkfzajf0.us-east-1.rds.amazonaws.com"
+DB_NAME = "postgres"
+DB_USER = "postgres"
+DB_PASS = "15512332"
+
+def login_required(func): # Lógica do parâmetro de login_required, onde escolhe quais páginas onde apenas o usuário logado pode acessar
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'loggedin' not in session:
+            return redirect(url_for('login'))
+        return func(*args, **kwargs)
+    return wrapper
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 
-    if request.method == 'POST':
-            return redirect(url_for('inicio'))
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cur.execute(
+            "SELECT * FROM calibracao.tb_usuario WHERE email = %s AND senha = %s", (username, password))
+        user = cur.fetchone()
+   
+        if user is not None:
+            session['loggedin'] = True
+            session['username'] = user['email']
+            return redirect(url_for('inicio'))
+        else:
+            pass
+    return render_template("login.html")
+
+@app.route('/logout')
+def logout(): # Botão de logout
+	session.pop('loggedin', None)
+	session.pop('id', None)
+	session.pop('username', None)
+	return redirect(url_for('login'))
 
 @app.route('/')
+@login_required
 def inicio(): 
     
     return render_template("home_calibracao.html")
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['GET','POST'])
+@login_required
 def cadastro(): 
-    
+
+    if request.method == 'POST':
+
+        equip = request.form['equipamento']
+        fabric = request.form['fabricante']
+        grandeza = request.form['grandeza']
+        unidade = request.form['unidade']
+        faixa_nominal = request.form['nominal']
+        faixa_calibracao = request.form['faixa_calibracao']
+        preco = request.form['preco']
+
+        print(equip,fabric,grandeza,unidade,faixa_nominal,faixa_calibracao,preco)
+
+    return render_template("cadastro.html")
+
+@app.route('/equipamento', methods=['GET','POST'])
+@login_required
+def equipamento(): 
+
+    if request.method == 'POST':
+
+        tag = request.form['tag']
+        tag_equipamento = request.form['tag_equipamento']
+        tag_nominal = request.form['tag_nominal']
+        tag_unidade = request.form['tag_unidade']
+        tag_localizacao = request.form['tag_localizacao']
+        tag_responsavel = request.form['tag_responsavel']
+        tag_controle = request.form['tag_controle']
+        tag_data = request.form['tag_data']
+        tag_periodicidade = request.form['tag_periodicidade']
+        tag_metodo = request.form['tag_metodo']
+
+        print(tag,tag_equipamento,tag_nominal,tag_unidade,tag_localizacao,tag_responsavel,tag_controle,tag_data,tag_periodicidade,tag_metodo)
+
     return render_template("cadastro.html")
 
 @app.route('/relacao')
+@login_required
 def relacao(): 
     
     return render_template("relacao.html")
