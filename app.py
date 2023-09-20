f30a08dd511bd56ed6494a6fb5d724dfd722c72b
TI <pcp@cemag.com.br>
2023-09-20 15:23:05 -0300

    unidade e faixa nominal filtrando conforme o equipamento

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, redirect, url_for, request, session, flash, make_response, Response
+from flask import Flask,render_template, redirect, url_for, request, session, flash, make_response, Response,jsonify  
 import psycopg2  # pip install psycopg2
 import psycopg2.extras
 from functools import wraps
@@ -56,6 +56,32 @@ def inicio():
     
     return render_template("home_calibracao.html")
 
+@app.route('/cadastro_equip', methods=['GET','POST'])
+@login_required
+def cadastro_equip(): 
+
+    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    if request.method == 'POST':
+
+        equipamento = request.form.get('equipamento')
+        fabricante = request.form.get('fabricante')
+        grandeza = request.form.get('grandeza')
+        unidade = request.form.get('unidade')
+        nominal = request.form.get('nominal')
+        faixa_calibracao = request.form.get('faixa_calibracao')
+        preco = request.form.get('preco')
+
+        cur.execute("INSERT INTO calibracao.tb_get_equipamentos (equipamento, fabricante, grandeza, unidade, faixa_nominal, faixa_calibracao, preco) VALUES (%s,%s,%s,%s,%s,%s,%s)",
+            (equipamento, fabricante, grandeza, unidade, nominal, faixa_calibracao, preco))
+            
+        conn.commit()
+
+        conn.close()
+
+    return render_template("cadastro.html")
+
 @app.route('/cadastro', methods=['GET','POST'])
 @login_required
 def cadastro(): 
@@ -66,12 +92,6 @@ def cadastro():
     if request.method == 'POST':
 
         equip = request.form['tag_equipamento']
-        # fabric = request.form['fabricante']
-        # grandeza = request.form['grandeza']
-        # unidade = request.form['unidade']
-        # faixa_nominal = request.form['nominal']
-        # faixa_calibracao = request.form['faixa_calibracao']
-        # preco = request.form['preco']
 
         query = (f"""   SELECT DISTINCT (unidade,faixa_nominal)
                         FROM calibracao.tb_get_equipamentos
@@ -81,42 +101,48 @@ def cadastro():
         data = cur.fetchall()
         df_data = pd.DataFrame(data)
         unidades_no_equipamento = df_data[0].values.tolist()
-        print(unidades_no_equipamento)
+        # print(unidades_no_equipamento)
 
-        return render_template("cadastro.html",unidades_no_equipamento=unidades_no_equipamento)
+        lista_unidades = []
+        lista_faixa_nominal = []
 
-    s = ("""SELECT DISTINCT(equipamento) 
+        for tupla in unidades_no_equipamento:
+            partes = tupla.strip('()').split(',')
+            elemento_0 = partes[0].strip('"')
+            elemento_1 = partes[1].strip('"')
+            
+            # Verifique se o elemento da posição 0 não é vazio e não está na lista
+            if elemento_0 and elemento_0 not in lista_unidades:
+                lista_unidades.append(elemento_0)
+            
+            # Verifique se o elemento da posição 1 não é vazio e não está na lista
+            if elemento_1 and elemento_1 not in lista_faixa_nominal:
+                lista_faixa_nominal.append(elemento_1)  
+
+        return jsonify({'unidades': lista_unidades, 'faixa_nominal': lista_faixa_nominal})
+
+    s = ("""SELECT DISTINCT equipamento, unidade 
             FROM calibracao.tb_get_equipamentos
             ORDER BY equipamento;""")
+    
+    query = (""" SELECT *
+                FROM tb_matriculas;""")
+    
+    cur.execute(query)
+    data_matricula = cur.fetchall()
+    df_data_matricula = pd.DataFrame(data_matricula)
+    lista_responsavel = df_data_matricula[2].values.tolist()
+    lista_matricula = df_data_matricula[1].values.tolist()
+    responsaveis = [f"{mat} - {resp}" for mat, resp in zip(lista_matricula,lista_responsavel)]
 
     cur.execute(s)
     data = cur.fetchall()
     df_data = pd.DataFrame(data)
     equipamentos = df_data[0].values.tolist()
+    unidades = df_data[1].values.tolist()
+    unidades = list(set(filter(None, unidades)))
     
-    return render_template("cadastro.html",equipamentos=equipamentos)
-
-@app.route('/cadastro_tag', methods=['GET','POST'])
-@login_required
-def cadastro_tag(): 
-
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-
-    if request.method == 'POST':
-
-        tag = request.form['tag']
-        tag_equipamento = request.form['tag_equipamento']
-        tag_nominal = request.form['tag_nominal']
-        tag_unidade = request.form['tag_unidade']
-        tag_localizacao = request.form['tag_localizacao']
-        tag_responsavel = request.form['tag_responsavel']
-        tag_controle = request.form['tag_controle']
-        tag_data = request.form['tag_data']
-        tag_periodicidade = request.form['tag_periodicidade']
-        tag_metodo = request.form['tag_metodo']
-
-    return render_template("cadastro.html")
+    return render_template("cadastro.html",equipamentos=equipamentos,responsaveis=responsaveis,unidades=unidades)
 
 @app.route('/relacao')
 @login_required
